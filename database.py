496e85c3a330428669fd9b5be8913a2efd0f7863
Rashed <rashedulb13@gmail.com>
2022-09-18 09:55:54 +0600

    solve getting all image issues

--- database.py
+++ database.py
@@ -1,32 +1,28 @@
-from enum import unique
-from unicodedata import name
-from webbrowser import BaseBrowser
 from peewee import *
 
-db = MySQLDatabase('mysql', user='mysql', password='mysql',
-                         host='127.0.0.1', port=3306)
+db = MySQLDatabase('scraping', user='Rashedul', password='R@shedu1',
+                   host='localhost', port=3306)
+# db.connect()
+# print(db)
 
-db.connect()
-print(db)
+
 class BaseModel(Model):
     class Meta:
         database = db
 
+
 class Hotel(BaseModel):
-    id=AutoField(primary_key = True)
-    name=CharField(max_length=200)
+    id = AutoField(primary_key=True)
+    name = CharField(max_length=200)
+
 
 class Label(BaseModel):
-    id=AutoField(primary_key = True)
-    name=CharField(max_length=50,unique=True)
+    id = AutoField(primary_key=True)
+    name = CharField(max_length=50, unique=True)
+
 
 class Images(BaseModel):
-    hotel= ForeignKeyField(Hotel)
-    image=CharField(max_length=200,unique=True)
-    label=ForeignKeyField(Label)
+    hotel = ForeignKeyField(Hotel)
+    image = CharField(max_length=200, unique=True)
+    label = ForeignKeyField(Label)
 
-db.create_tables([Hotel, Label,Images])
-
-res=Label.create(name="Bedroom")
-
-print(res)
--- index.py
+++ index.py
@@ -1,97 +1,129 @@
+from traceback import print_tb
 from selenium.webdriver.firefox.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import time
 import string
+from database import db, Label, Images, Hotel
+
 option = Options()
-option.headless=False
-service = Service(executable_path="/home/w3e103/web_drivers/geckodriver")
-driver = webdriver.Firefox(service=service,options=option)
+option.headless = False
+office = "/home/w3e103/web_drivers/geckodriver"
+personal = "C:\\Users\\rashe\\src\\geckodriver.exe"
+service = Service(executable_path=personal)
+driver = webdriver.Firefox(service=service, options=option)
 driver.fullscreen_window()
 
-# #strip number from string
-# def stripNumber(label:str):
-#     return label.rstrip(string.digits)
-#get a link for given destination
-def getDestinationLink(destination:str):
+
+def getDestinationLink(destination: str):
     driver.get('https://www.kayak.co.in/?ispredir=true')
-    elements= driver.find_elements(by=By.CLASS_NAME,value='P_Ok-wrapper')
+    elements = driver.find_elements(by=By.CLASS_NAME, value='P_Ok-wrapper')
     for element in elements:
-        place=element.find_element(by=By.CLASS_NAME, value='P_Ok-title').text
-        if(place.lower() == destination.lower()):
-            anchor= element.find_element(by=By.CSS_SELECTOR, value='.P_Ok-header-links span:last-child a')
+        place = element.find_element(by=By.CLASS_NAME, value='P_Ok-title').text
+        if (place.lower() == destination.lower()):
+            anchor = element.find_element(
+                by=By.CSS_SELECTOR, value='.P_Ok-header-links span:last-child a')
     return anchor.get_attribute(name='href')
 
 
-def getHotelNameAndLink(url:str):
+def getHotelNameAndLink(url: str):
     try:
         driver.get(url)
     except:
         print('error while loading page')
-        return None 
-    anchor=driver.find_element(by=By.CLASS_NAME,value='soom-name')
-    hotelName = anchor.find_element(by=By.CSS_SELECTOR,value='span').text
-    return (anchor.get_attribute(name='href'),hotelName)
+        return None
+    anchor = driver.find_element(by=By.CLASS_NAME, value='soom-name')
+    hotelName = anchor.find_element(by=By.CSS_SELECTOR, value='span').text
+    return (anchor.get_attribute(name='href'), hotelName)
 
 
 def getAHotel(url):
     driver.get(url)
     for btn in driver.find_elements(by=By.TAG_NAME, value='button'):
         try:
-            if 'view all photos'.lower() == btn.find_element(by=By.CSS_SELECTOR,value='.Iqt3-button-content').text.lower():
+            if 'view all photos'.lower() == btn.find_element(by=By.CSS_SELECTOR, value='.Iqt3-button-content').text.lower():
                 btn.click()
-                labelButtons = driver.find_element(by=By.CLASS_NAME,value='DTct-categories-container').find_elements(by=By.TAG_NAME, value='button')
-                buttonWithLabel = [(button.find_element(by=By.CSS_SELECTOR, value='div div div').text.rstrip(string.digits), button) for button in labelButtons]
+                labelButtons = driver.find_element(
+                    by=By.CLASS_NAME, value='DTct-categories-container').find_elements(by=By.TAG_NAME, value='button')
+                buttonWithLabel = [(button.find_element(by=By.CSS_SELECTOR, value='div div div').text.rstrip(
+                    string.digits), button) for button in labelButtons]
                 return buttonWithLabel[1:]
         except:
             print('View all button not found')
-        
+
     # for button in labelButtons:
  # take tuple list of button and label and return a dictionary containing label and corresponding image list
  # dict={
  # 'label1': [img1,img2],
- # 'label-2': [img1,img2,img3]  
- # }      
-def getImageWithLabel(buttonWithLabel:list)-> dict:
-    obj={}
+ # 'label-2': [img1,img2,img3]
+ # }
+
+
+def getImageWithLabel(buttonWithLabel: list) -> dict:
+    obj = {}
+    # iterate on every button and click those button in every iteration
     for button in buttonWithLabel:
-        # time.sleep(2)
         print(button[0])
-        label = button[0].split(' ')[0]
+        label = button[0]
+        labelFirstWord = button[0].split(' ')[0]
         try:
             button[1].click()
+            time.sleep(3)
+            obj[label] = []
         except:
             print('button is not clickable')
-            print(button[1])
             continue
-        images = driver.find_elements(by=By.CSS_SELECTOR, value='.ZVFD-dots-container img')
-        if len(images)>1:
-            for image in images:
-                link = image.get_attribute('src')
-                print(link)
-                if link != None:
-                    try:
-                        obj[button[0]]+= [link]
-                    except KeyError:
-                        obj[button[0]] = [link]
-                
-        else:
-            image = driver.find_element(by=By.CSS_SELECTOR, value=f'img[alt*={label}]').get_attribute('src')
-            print(image)
-            obj[button[0]] = [image]
+
+        try:
+            firstListItem = driver.find_element(
+                by=By.CSS_SELECTOR, value='.ZVFD-dots-wrapper')
+            driver.execute_script(
+                f'arguments[0].style.transform = "translateX(-0%)"', firstListItem)
+
+        except:
+            print('not found')
+
+        try:
+            buttons = driver.find_elements(
+                by=By.CSS_SELECTOR, value='.ZVFD-dots-wrapper li button')
+            # print(len(buttons))
+            # firstButton = button[0]
+            if len(buttons) > 0:
+                for button in buttons:
+                    # print(button)
+                    button.click()
+                    time.sleep(1)
+                    image = button.find_element(
+                        by=By.TAG_NAME, value='img').get_attribute('src')
+                    obj[label] += [image]
+
+            else:
+                image = driver.find_element(
+                    by=By.CSS_SELECTOR, value=f'img[alt*={labelFirstWord}]').get_attribute('src')
+                obj[label] = [image]
+        except:
+            print('error happened')
+
     return obj
 
 
-#program start executing from here
+# program start executing from here
 def main():
-    hotelName = ''
-    dest=getDestinationLink('kolkata')
-    hotel=getHotelNameAndLink(dest)
-    hotelName=hotel[1]
-    hotelUrl=hotel[0]
-    image=getImageWithLabel(getAHotel(hotelUrl))
-    
-    print(image)
-    
+    # hotelName = ''
+    # dest = getDestinationLink('kolkata')
+    # hotel = getHotelNameAndLink(dest)
+    # hotelName = hotel[1]
+    # hotelUrl = hotel[0]
+    # image = getImageWithLabel(getAHotel(hotelUrl))
+
+    # print(image)
+    time.sleep(5)
+    driver.close()
+    db.connect()
+    db.create_tables([Hotel, Label, Images])
+
+    res = Label.create(name="Bedroom")
+
+
 main()
